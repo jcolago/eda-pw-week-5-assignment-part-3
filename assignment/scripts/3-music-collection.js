bfa5e07dce26e5fc9798f3f9084085169c870a93
jcolago <jcolago2005@gmail.com>
2023-08-17 15:37:47 -0500

    add 'addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,17 @@
 console.log('***** Music Collection *****')
 //This will be the basis for our music array.
+// We will be adding to this array via the later functions.
 const collection=[];
+
+//This function will add an album's title, artist and year published to a new object and then add that object to the collection array.
+
+function addToCollection(title,artist, yearPublished){
+    const album={};
+    album.title = title;
+    album.artist = artist;
+    album.yearPublished = yearPublished;
+    collection.push(album)
+    return console.log('This album was added to the collection', album)
+}
+
+addToCollection('Distant Worlds: Music from Final Fantasy', 'Nobuo Uematsu', '2007')
