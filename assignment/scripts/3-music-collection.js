16dc8549053858b3bb3cadd02dbaaa77388015a4
jcolago <jcolago2005@gmail.com>
2023-08-19 16:48:20 -0500

    This will add showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,18 +10,27 @@ function addToCollection(title,artist, yearPublished){
     album.title = title;
     album.artist = artist;
     album.yearPublished = yearPublished;
-    collection.push(album)
-    return console.log('This album was added to the collection', album)
+    collection.push(album);
+    return console.log('This album was added to the collection', album);
 }
 //This tests the addToCollection function.
 //This section also adds 6 albums to the collection.
 
-addToCollection('Distant Worlds: Music from Final Fantasy', 'Nobuo Uematsu', '2007')
-addToCollection('Heroes', 'The Midnight', '2022')
-addToCollection('Brand New Eyes', 'Paramour', '2009')
-addToCollection('Monsters', 'The Midnight', '2020')
-addToCollection('Viva La Vida or Death and All His Friend', 'Coldplay', '2008')
-addToCollection('head or heart', 'Christina Perri', '2014')
+addToCollection('Distant Worlds: Music from Final Fantasy', 'Nobuo Uematsu', '2007');
+addToCollection('Heroes', 'The Midnight', '2022');
+addToCollection('Brand New Eyes', 'Paramour', '2009');
+addToCollection('Monsters', 'The Midnight', '2020');
+addToCollection('Viva La Vida or Death and All His Friend', 'Coldplay', '2008');
+addToCollection('head or heart', 'Christina Perri', '2014');
 
 //This section will log the collection array.
 console.log(collection)
+
+function showCollection(Array){
+    for (let i=0; i<Array.length; i++){ 
+    }
+    Array.forEach(({title, artist, yearPublished})=>{console.log(`${title} by ${artist}, published in ${yearPublished}.`)});
+}
+
+
+console.log(showCollection(collection));
